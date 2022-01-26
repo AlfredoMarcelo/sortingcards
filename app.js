590a1bb39ae252b8bf58afcc3206e4bd050cbe2c
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-01-25 23:02:28 -0300

    listas de cartas creadas

--- app.js
+++ app.js
@@ -6,5 +6,30 @@
   console.log(i + 1 + ". Hola desde i");
 }
  */
+let cards = ["&#9829;", "&#9827;", "&#9830;", "&#9824;"];
+let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",];
+let colors = ["text-danger", "text-dark"];
+console.log(cards);
+console.log(numbers);
+console.log(colors);
 
-let arreglo = [45, 2, 3, 14, 11];
+/* 
+function displayCards() {
+  let singleCard = cards[Math.floor(Math.random() * cards.length)];
+  let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let changeColor = colors[Math.floor(Math.random() * colors.length)];
+  let showNumber = singleNumber;
+  let showCard = singleCard;
+  let showColor = changeColor;
+  const number = document.getElementById("number");
+  number.innerHTML = showNumber;
+  const iconTop = document.getElementById("iconTop");
+  iconTop.innerHTML = showCard;
+  iconTop.classList.add(showColor);
+  const iconButtom = document.getElementById("iconButtom");
+  iconButtom.classList.add(showColor);
+  iconButtom.innerHTML = showCard;
+}
+displayCards();
+const button = document.querySelector("button");
+button.addEventListener("click", () => displayCards()); */
