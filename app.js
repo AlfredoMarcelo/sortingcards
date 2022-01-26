d4b44b501f6e4eff5c08378725af212591263f0f
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-01-25 21:08:10 -0300

    avance en template de cartas

---
+++ app.js
@@ -0,0 +1,10 @@
+/* for (let i = 0; i < 10; i++) {
+  //imprime el for de a 10 veces osea 10 veces 10  hola desde a.
+  for (let a = 0; a < 10 - i; a++) {
+    console.log(a + 1 + ". hola desde a"); //imprime 10
+  }
+  console.log(i + 1 + ". Hola desde i");
+}
+ */
+
+let arreglo = [45, 2, 3, 14, 11];
